8395ac4868663363aaf136b468593c1008913ccf
antonovmike <antonovma@outlook.com>
2026-03-23 10:23:30 +0100

    [REF] Add const host

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,10 +1,12 @@
+const host = "http://localhost:3000/";
+
 export async function getState() {
-  const res = await fetch("http://localhost:3000/state");
+  const res = await fetch(`${host}state`);
   return res.json();
 }
 
 export async function makeChoice(index: number) {
-  const res = await fetch("http://localhost:3000/choice", {
+  const res = await fetch(`${host}choice`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ index }),
@@ -13,7 +15,7 @@ export async function makeChoice(index: number) {
 }
 
 export async function reset() {
-  const res = await fetch("http://localhost:3000/reset", {
+  const res = await fetch(`${host}reset`, {
     method: "POST",
   });
   return res.json();
